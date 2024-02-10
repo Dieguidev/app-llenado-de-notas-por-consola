73e5a78bafd8592e1c6c920f1b4497987cba4331
Dieguidev <diegogaraycullas@gmail.com>
2024-02-10 10:53:29 -0500

    feat: Implementar lógica principal de la aplicación en app.js
    - Importadas las funciones necesarias para interactuar con el usuario y guardar datos en un archivo
    - Creada una instancia de la clase Notas para gestionar las notas
    - Implementado un bucle principal que muestra un menú interactivo y permite al usuario realizar acciones como crear notas
    - Utilizado el método de la clase Notas para crear una nueva nota con la entrada del usuario
    - Incluida la lógica para pausar la ejecución después de cada acción realizada por el usuario

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
+const { saveDB } = require('./helpers/SaveFile');
 const { inquirerMenu, readInput, pause } = require('./helpers/inquirer');
+const Notas = require('./models/notas');
 
 const main = async () => {
   let opt = '';
 
+  const notas = new Notas();
+
+  if (opt !== '0') await pause();
+
   do {
     opt = await inquirerMenu();
 
-    // switch (opt) {
-    //   case '1':
-    //     const desc = await readInput('Curso y nota: ');
-    //     break;
+    switch (opt) {
+      case '1':
+        const desc = await readInput('Curso y nota: ');
+        notas.crearNota(desc);
+        break;
+      case '2':
+        break;
+    }
 
-    //   default:
-    //     break;
-    // }
     if (opt !== '0') await pause();
-  } while (opt !== 0);
+
+    // saveDB(notas.listadoArr);
+  } while (opt != 0);
 };
 
 main();
