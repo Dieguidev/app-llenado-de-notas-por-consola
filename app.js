5f9d25c456f832be947155a161319c4e566a08f1
Dieguidev <diegogaraycullas@gmail.com>
2024-02-10 08:23:48 -0500

    feat: Agregar clase Nota para gestionar notas
    - Implementada la clase Nota para representar una nota con un identificador único, descripción y fecha de creación
    - Se utiliza la librería uuid para generar identificadores únicos
    - La clase asigna automáticamente la fecha de creación al instanciarse

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
+const { inquirerMenu, readInput, pause } = require('./helpers/inquirer');
 
+const main = async () => {
+  let opt = '';
 
+  do {
+    opt = await inquirerMenu();
+
+    // switch (opt) {
+    //   case '1':
+    //     const desc = await readInput('Curso y nota: ');
+    //     break;
+
+    //   default:
+    //     break;
+    // }
+    if (opt !== '0') await pause();
+  } while (opt !== 0);
+};
+
+main();
